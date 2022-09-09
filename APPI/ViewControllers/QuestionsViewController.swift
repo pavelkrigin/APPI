1ef0d879dc657765b08848ec17703e19c08544ed
Ilya Osipov <osiios.dev@gmail.com>
2022-09-09 13:52:42 +0300

    1. StackViews for question pages have been added 2. Extension for QuestionsViewController has been added

--- APPI/ViewControllers/QuestionsViewController.swift
+++ APPI/ViewControllers/QuestionsViewController.swift
@@ -9,17 +9,29 @@ import UIKit
 
 final class QuestionsViewController: UIViewController {
     // MARK: - IBOutlets
+    @IBOutlet var colorStackView: UIStackView!
+    @IBOutlet var colorQuestion: UILabel!
     @IBOutlet var colorImage: UIImageView!
+    @IBOutlet var colorAnswers: [UIButton]!
+    
+    @IBOutlet var foodStackView: UIStackView!
+    @IBOutlet var foodQuestion: UILabel!
     @IBOutlet var foodImage: UIImageView!
+    @IBOutlet var foodAnswers: [UIButton]!
+    
+    @IBOutlet var hobbyStackView: UIStackView!
+    @IBOutlet var hobbyQuestion: UILabel!
     @IBOutlet var hobbyImage: UIImageView!
+    @IBOutlet var hobbyAnswers: [UIButton]!
+    
+    @IBOutlet var movieStackView: UIStackView!
+    @IBOutlet var movieQuestion: UILabel!
     @IBOutlet var movieImage: UIImageView!
+    @IBOutlet var movieAnswers: [UIButton]!
     
-    @IBOutlet var colorQuestions: [UIButton]!
-    @IBOutlet var foodQuestions: [UIButton]!
-    @IBOutlet var hobbyQuestions: [UIButton]!
-    @IBOutlet var movieQuestions: [UIButton]!
+    // MARK: - Properties
+    private let questions = Question.getQuestions()
     
-    // MARK: - Private properties
     var questionCatImage: String?
     
     // MARK: - Lifecycle
@@ -30,7 +42,31 @@ final class QuestionsViewController: UIViewController {
         foodImage.image = UIImage(named: questionCatImage ?? "food")
         hobbyImage.image = UIImage(named: questionCatImage ?? "hobby")
         movieImage.image = UIImage(named: questionCatImage ?? "movie")
-
+        
+        updateUserInterface()
     }
     
+    // MARK: - IBActions
+    @IBAction func colorAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func foodAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func hobbyAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func movieAnswerButtonPressed(_ sender: UIButton) {
+    }
 }
+
+    // MARK: - Extantions
+    extension QuestionsViewController {
+        private func updateUserInterface() {
+            let stackViews = [colorStackView, foodStackView, hobbyStackView, movieStackView]
+            
+            for stackView in stackViews {
+                stackView?.isHidden = true
+            }
+        }
+    }
