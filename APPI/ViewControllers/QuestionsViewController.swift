0985b6926705ea95763c1f98786149e0207e5cda
Ilya Osipov <osiios.dev@gmail.com>
2022-09-09 12:41:08 +0300

    1. IBOutlets for questionButtons have been added

--- APPI/ViewControllers/QuestionsViewController.swift
+++ APPI/ViewControllers/QuestionsViewController.swift
@@ -14,13 +14,22 @@ final class QuestionsViewController: UIViewController {
     @IBOutlet var hobbyImage: UIImageView!
     @IBOutlet var movieImage: UIImageView!
     
+    @IBOutlet var colorQuestions: [UIButton]!
+    @IBOutlet var foodQuestions: [UIButton]!
+    @IBOutlet var hobbyQuestions: [UIButton]!
+    @IBOutlet var movieQuestions: [UIButton]!
+    
     // MARK: - Private properties
     var questionCatImage: String?
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorImage.image = UIImage(named: questionCatImage ?? "color")
+        foodImage.image = UIImage(named: questionCatImage ?? "food")
+        hobbyImage.image = UIImage(named: questionCatImage ?? "hobby")
+        movieImage.image = UIImage(named: questionCatImage ?? "movie")
 
     }
     
