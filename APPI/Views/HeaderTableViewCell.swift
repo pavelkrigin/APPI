5508f4b091e3279e111919a584f5c4f1b9e9a03e
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-12-19 19:04:11 +0300

    Initial Commit

---
+++ APPI/Views/HeaderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderTableViewCell.swift
+//  APPI
+//
+//  Created by mac on 9/9/22.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    
+}
